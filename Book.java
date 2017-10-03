84f2cce0c8c1f0264fa4ef036d89018471a8337e
MeetRami <meetrams45@gmail.com>
2017-10-03 13:11:11 -0400

    Lab2 2.85
    question 2.85 done

--- Book.java
+++ Book.java
@@ -11,15 +11,17 @@ class Book
     // The fields.
     private String author;
     private String title;
+    private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
 
     public String getAuthor()
@@ -32,6 +34,11 @@ class Book
         return title;
     }
     
+    public int getPages()
+    {
+        return pages;
+    }
+    
     public void printAuthor()
     {
         System.out.println("Author of the Book is: " + author);
@@ -41,4 +48,8 @@ class Book
     {
         System.out.println("Title of the Book is: " + title);
     }
+    
+    
+    
+    
 }
